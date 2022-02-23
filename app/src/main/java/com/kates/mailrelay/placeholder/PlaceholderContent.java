fbb587f004f577d89209754cde410ed3a0dbc8ff
davidhkates <davidhkates@yahoo.com>
2022-02-23 05:01:37 -0700

    Changed count to 12

--- app/src/main/java/com/kates/mailrelay/placeholder/PlaceholderContent.java
+++ app/src/main/java/com/kates/mailrelay/placeholder/PlaceholderContent.java
@@ -23,7 +23,7 @@ public class PlaceholderContent {
      */
     public static final Map<String, PlaceholderItem> ITEM_MAP = new HashMap<String, PlaceholderItem>();
 
-    private static final int COUNT = 10;
+    private static final int COUNT = 12;
 
     static {
         // Add some sample items.
